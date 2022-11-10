ee7948dca7201d893e41313e6dec35514bf9f54f
Ilya Druz <druzilya@ukr.net>
2022-11-11 00:11:27 +0200

    Add models

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["country"]
+
+    def __str__(self):
+        return f"Name: {self.name} | Country: {self.country}"
+
+
+class Account(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+    phone = models.IntegerField(unique=True)
+    address = models.CharField(max_length=255, unique=True)
+    name_pharmacy = models.CharField(max_length=255)
+    time_work = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "account"
+        verbose_name_plural = "accounts"
+
+    def __str__(self):
+        return (
+            f"{self.username} ({self.first_name}"
+            f" {self.last_name}, licence number: {self.license_number}"
+        )
+
+    def get_absolute_url(self):
+        return reverse("pharmacy:account-detail", kwargs={"pk": self.pk})
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    manufacturer = models.ForeignKey(
+        Manufacturer, on_delete=models.CASCADE, related_name="medicines"
+    )
+    accounts = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="medicines"
+    )
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    amount_medicine = models.IntegerField()
+
+    class Meta:
+        ordering = ["-amount_medicine"]
+
+    def __str__(self):
+        return f"Model: {self.name}; Manufacturer: {self.manufacturer.name}"

c539c07045f615e5414e97016013b3a26dfeac25
Ilya Druz <druzilya@ukr.net>
2022-11-11 00:20:44 +0200

    Add admin.py

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from pharmacy.models import Manufacturer, Account, Medicine
+
+
+admin.site.register(Manufacturer)
+
+
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ["model", "manufacturer"]
+    list_filter = ["manufacturer"]
+    search_fields = ["model"]
+
+
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "license_number",
+                )
+            },
+        ),
+    )

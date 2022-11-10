fa308a143893ec5c04039b0e52268ca8979954a8
Ilya Druz <druzilya@ukr.net>
2022-11-11 00:44:17 +0200

    Create url in app, templates and HomePage

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from pharmacy.models import Account, Medicine, Manufacturer
+
+
+@login_required
+def index(request):
+    num_accounts = Account.objects.count()
+    num_medicines = Medicine.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_accounts": num_accounts,
+        "num_medicines": num_medicines,
+        "num_manufacturers": num_manufacturers,
+        "num_visits": num_visits + 1
+    }
+
+    return render(request, "pharmacy/index.html", context=context)
